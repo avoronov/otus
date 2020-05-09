0b52a1d6be62096de3d18a81001fbe0567cb5a92
Alexander Voronov <you@example.comq>
2020-05-09 11:08:09 +0300

    fixes on code review comments

--- LearningCenter/learningcenter/restapi/urls.py
+++ LearningCenter/learningcenter/restapi/urls.py
@@ -1,26 +1,26 @@
 from django.urls import path
 
-from .views import CourseList, CourseDetail, CourseCreate, CourseUpdate, CourseDelete, TeacherList, TeacherCreate, \
-    TeacherDetail, TeacherUpdate, TeacherDelete, StudentList, StudentCreate, StudentDetail, StudentUpdate, StudentDelete
+from .views import CourseViewSet, TeacherViewSet, StudentViewSet
+
 
 urlpatterns = [
-    path('courses/', CourseList.as_view()),
-    path('courses/add', CourseCreate.as_view()),
-    path('courses/<int:pk>/', CourseDetail.as_view()),
-    path('courses/<int:pk>/update', CourseUpdate.as_view()),
-    path('courses/<int:pk>/delete', CourseDelete.as_view()),
+    path('courses/', CourseViewSet.as_view({'get': 'list'})),
+    path('courses/add', CourseViewSet.as_view({'post': 'create'})),
+    path('courses/<int:pk>/', CourseViewSet.as_view({'get': 'retrieve'})),
+    path('courses/<int:pk>/update', CourseViewSet.as_view({'put': 'update', 'patch': 'partial_update'})),
+    path('courses/<int:pk>/delete', CourseViewSet.as_view({'delete': 'destroy'})),
 
-    path('teachers/', TeacherList.as_view()),
-    path('teachers/add', TeacherCreate.as_view()),
-    path('teachers/<int:pk>/', TeacherDetail.as_view()),
-    path('teachers/<int:pk>/update', TeacherUpdate.as_view()),
-    path('teachers/<int:pk>/delete', TeacherDelete.as_view()),
+    path('teachers/', TeacherViewSet.as_view({'get': 'list'})),
+    path('teachers/add', TeacherViewSet.as_view({'post': 'create'})),
+    path('teachers/<int:pk>/', TeacherViewSet.as_view({'get': 'retrieve'})),
+    path('teachers/<int:pk>/update', TeacherViewSet.as_view({'put': 'update', 'patch': 'partial_update'})),
+    path('teachers/<int:pk>/delete', TeacherViewSet.as_view({'delete': 'destroy'})),
 
-    path('students/', StudentList.as_view()),
-    path('students/add', StudentCreate.as_view()),  # TODO: teacher is added instead of student, why?
-    path('students/<int:pk>/', StudentDetail.as_view()),
-    path('students/<int:pk>/update', StudentUpdate.as_view()),
-    path('students/<int:pk>/delete', StudentDelete.as_view()),
+    path('students/', StudentViewSet.as_view({'get': 'list'})),
+    path('students/add', StudentViewSet.as_view({'post': 'create'})),  # TODO: teacher is added instead of student, why?
+    path('students/<int:pk>/', StudentViewSet.as_view({'get': 'retrieve'})),
+    path('students/<int:pk>/update', StudentViewSet.as_view({'put': 'update', 'patch': 'partial_update'})),
+    path('students/<int:pk>/delete', StudentViewSet.as_view({'delete': 'destroy'})),
 
     # todo:
     # - assign/unassign teacher to/from course
--- LearningCenter/learningcenter/restapi/views.py
+++ LearningCenter/learningcenter/restapi/views.py
@@ -1,93 +1,22 @@
 from courses.models import Course, Teacher, Student
-from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 from restapi.serializers import CourseSerializer, TeacherSerializer, StudentSerializer
 
 
-class CourseList(generics.ListAPIView):
-    permission_classes = (permissions.AllowAny,)
+class CourseViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
-class CourseDetail(generics.RetrieveAPIView):
-    permission_classes = (permissions.AllowAny,)
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
-class CourseCreate(generics.CreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
-class CourseUpdate(generics.UpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
-class CourseDelete(generics.DestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
-class TeacherList(generics.ListAPIView):
-    permission_classes = (permissions.AllowAny,)
+class TeacherViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
 
 
-class TeacherDetail(generics.RetrieveAPIView):
-    permission_classes = (permissions.AllowAny,)
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-
-
-class TeacherCreate(generics.CreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-
-
-class TeacherUpdate(generics.UpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-
-
-class TeacherDelete(generics.DestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
-
-
-class StudentList(generics.ListAPIView):
-    permission_classes = (permissions.AllowAny,)
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
-class StudentDetail(generics.RetrieveAPIView):
-    permission_classes = (permissions.AllowAny,)
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
-class StudentCreate(generics.CreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
-class StudentUpdate(generics.UpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
-class StudentDelete(generics.DestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class StudentViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
